a8aced454d739c014ea35c31922433ac9d4977f2
Gonzalo Patricio Di Ascenzi Severini <Gonzalo.Di.Ascenzi.-ND@disney.com>
2023-02-22 17:48:04 -0300

    First Week

--- lib/movie/repositories/movie_repository.dart
+++ lib/movie/repositories/movie_repository.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import '../models/movie_model.dart';
-import '../models/movie_video_model.dart';
 
 abstract class MovieRepository {
   Future<Either<String, MovieResponseModel>> getDiscover({int page = 1});
